6c2616599ffb54b88eed2a16778388834d1c7ed2
今安在 <zzq@MacBook-Pro.local>
2024-10-08 14:19:54 +0800

    Site updated: 2024-10-08 14:19:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/07/Blog/","2024/10/07/hello-world/"];function toRandomPost(){
+var posts=["2024/10/07/Blog/","2024/10/08/AI课程建设试试水/","2024/10/07/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
